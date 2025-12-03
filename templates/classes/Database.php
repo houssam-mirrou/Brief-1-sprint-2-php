10d5a534a7e5b0449172bbc3553ba19479a5f2b2
houssam-mirrou <hossam.mirrou@gmail.com>
2025-12-03 14:41:38 +0100

    feat/storing_comments storing the comments in the table comments

--- templates/classes/Database.php
+++ templates/classes/Database.php
@@ -4,29 +4,21 @@ class Database
 {
     public $connection;
 
-    public function __construct()
+    public function __construct($config, $username = 'root', $password = '')
     {
-        $config = [
-            'host' => 'localhost',
-            'port' => 3306,
-            'dbname' => 'myapp',
-            'charset' => 'utf8mb4'
-        ];
-
         $dsn = 'mysql:' . http_build_query($config, '', ';');
-        $username = "root";
-        $password = "";
-        
         $this->connection = new PDO($dsn, $username, $password, [
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]);
     }
 
-    public function query($query)
+    public function query($query, $params = [])
     {
         $statement = $this->connection->prepare($query);
-        $statement->execute();
-
-        return $statement->fetchAll();
+        $statement->execute($params);
+        if (str_starts_with(strtolower(trim($query)), "select")) {
+            return $statement->fetchAll();
+        }
+        return true;
     }
 }
