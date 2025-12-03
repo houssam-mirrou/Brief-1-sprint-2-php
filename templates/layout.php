9c61182ab26727cd78680defeee1ab35af1b15f4
houssam-mirrou <hossam.mirrou@gmail.com>
2025-12-03 11:59:13 +0100

    feat/valid_champ ajout de validation des champ

--- templates/layout.php
+++ templates/layout.php
@@ -6,21 +6,68 @@ require 'classes/Comment.php';
 
 echo '<main class="flex-1">';
 
-function valider_name($name){
+function valider_name($name)
+{
+    if($name === null){
+        return false;
+    }
     $regex = "/^[A-Za-z]{3,}$/";
-    if(preg_match($name,$regex)){
+    if (preg_match($regex, $name)) {
         return true;
     }
     return false;
 }
-function valider_email($email){
-    $regex = "/^[^\s@]+@[^\s@]+\.[^\s@]+$/";
-
+function valider_email($email)
+{
+    if($email === null){
+        return false;
+    }
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
 }
-function valider_description($description){
-    $regex = "/^[a-zA-Z ]{20,}$/";
+function valider_description($description)
+{
+    if($description === null){
+        return false;
+    }
+    $regex = "/^.{20,}$/s";
+    if (preg_match($regex, $description)) {
+        return true;
+    }
+    return false;
 }
 
+function valider_les_champ($name, $email, $description)
+{
+    if (!valider_name($name)) {
+        echo '
+            <script>
+                const name_input = document.querySelector(".username");
+                console.log(name_input);
+                name_input.focus();
+            </script>
+        ';
+        return false;
+    }
+    if (!valider_email($email)) {
+        echo '
+            <script>
+                const email_input = document.querySelector(".email");
+                email_input.focus();
+            </script>
+        ';
+        return false;
+    }
+    if (!valider_description($description)) {
+        echo '
+            <script>
+                const description_input = document.querySelector(".description");
+                description_input.focus();
+            </script>
+        ';
+        return false;
+    }
+    return true;
+}
 
 
 switch ($page_name) {
@@ -62,20 +109,59 @@ switch ($page_name) {
         break;
 
     case '/contact':
-        echo '<section class="container mx-auto py-16">
-                <h2 class="text-3xl font-bold mb-6 text-center">Contactez-nous</h2>
-                <form class="max-w-xl mx-auto bg-white p-8 shadow-md rounded-lg space-y-4" method="post">
-                    <input type="text" placeholder="Votre nom" class="w-full border px-4 py-2 rounded-lg" name="username">
-                    <input type="email" placeholder="Votre email" class="w-full border px-4 py-2 rounded-lg" name="email">
-                    <textarea placeholder="Votre message" class="w-full border px-4 py-2 rounded-lg" name="description"></textarea>
-                    <button class="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700" name="password">Envoyer</button>
-                </form>
-            </section>';
+        $tableaux = [
+            'username' => null,
+            'email' => null,
+            'description' => null
+        ];
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            $tableaux['username'] = $_POST["username"];
+            $tableaux['email'] = $_POST["email"];
+            $tableaux['description'] = $_POST["description"];
+            if (valider_name($tableaux['username']) && valider_email($tableaux['email']) && valider_description($tableaux['description'])) {
+                $tableaux['username'] = null;
+                $tableaux['email'] = null;
+                $tableaux['description'] = null;
+            }
+        }
 
-        
-        $username = $_POST["username"];
-        $email = $_POST["email"];
-        $description = $_POST["description"];
+?>
+        <section class="container mx-auto py-16">
+            <h2 class="text-3xl font-bold mb-6 text-center">Contactez-nous</h2>
+            <form class="max-w-xl mx-auto bg-white p-8 shadow-md rounded-lg space-y-4" method="post">
+            <input type="text" placeholder="Votre nom" class="w-full border px-4 py-2 rounded-lg username" name="username" value= <?= $tableaux["username"] ?>>
+            <?php
+                if (!valider_name($tableaux['username']) && $tableaux['username']!== null) {
+                    echo "<h2 class= 'text-xl text-[red]'>Tu doit ecrire plus de deux carachter</h2>";
+                }
+
+            ?>
+            <input type="email" placeholder="Votre email" class="w-full border px-4 py-2 rounded-lg email" name="email" value= <?= $tableaux["email"] ?>>
+            <?php
+            if (!valider_email($tableaux['email']) && $tableaux['email']!== null) {
+                echo "<h2 class= 'text-xl text-[red]'>Tu doit ecrire un email valide</h2>";
+            }
+            ?>
+            <textarea placeholder="Votre message" class="w-full border px-4 py-2 rounded-lg description" name="description"><?php echo $tableaux["description"] ?></textarea>
+            <?php
+            if (!valider_description($tableaux['description']) && $tableaux['description']!== null) {
+                echo "<h2 class= 'text-xl text-[red]'>Tu doit ecrire une description plus que 20 charachter</h2>";
+            }
+            ?>
+            <button class="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700" name="password">Envoyer</button>
+            </form>
+        </section>'
+<?php
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            $username = $_POST["username"];
+            $email = $_POST["email"];
+            $description = $_POST["description"];
+            if (valider_les_champ($username, $email, $description) === true) {
+                //ajouter un commenter dans le database;
+            } else {
+            }
+        }
+
 
         $data = new Database();
         $comments = $data->query('select * from comments');
@@ -89,7 +175,7 @@ switch ($page_name) {
                     <h2 class="text-2xl mb-3">' . $comment['name'] . '</h2>
                     <h2 class="text-2xl font-bold mb-3">Email : </h2>
                     <h2 class="text-2xl mb-3">' . $comment['email'] . '</h2>
-                    <h2 class="text-2xl font-bold mb-3">Description</h2>
+                    <h2 class="text-2xl font-bold mb-3">Message</h2>
                     <h2 class="text-2xl mb-3">' . $comment['descrip'] . '</h2>
                 </div>
             </section>';
